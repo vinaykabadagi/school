cf1df54e69cc01820ece7df8948d1149bd599657
Vinay Kabadagi <vinaykabadagi@gmail.com>
2021-06-12 19:19:13 +0530

    1

--- student/models.py
+++ student/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class student(models.Model):
     name=models.CharField(max_length=20,null=False)
-    standard=models.IntegerField(null=False)
+    standard=models.IntegerField(null=False,min_value=1,max_value=12)
     def __str__(self):
         return self.name
