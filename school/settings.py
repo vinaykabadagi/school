4a7292e1de5f22c8c3a1314dbb14fe677de1cebf
Vinay Kabadagi <vinaykabadagi@gmail.com>
2021-06-13 16:07:56 +0530

    5

--- school/settings.py
+++ school/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'school.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sample',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
