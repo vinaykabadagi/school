27f413f0d271bbf04a5e341ea3919ece86611052
Vinay Kabadagi <vinaykabadagi@gmail.com>
2021-06-13 13:29:20 +0530

    3

--- student/forms.py
+++ student/forms.py
@@ -2,9 +2,8 @@ from django import forms
 
 from .models import student
 class studentForm(forms.ModelForm): 
-    # specify the name of model to use 
     class Meta: 
         model = student 
         fields = ['name','standard']
         name = forms.CharField()
-        standard=forms.IntegerField()
+        standard=forms.CharField()
